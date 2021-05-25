db88dc08e72fa476a5aa19871ee41cf9df5241a1
oomsinsukit <oomsinsukit@gmail.com>
2021-05-25 17:53:16 +0700

    Add files via upload

---
+++ Exercise4_Sukit_K.py
@@ -0,0 +1,9 @@
+F_English = 80.5
+G_Business = 75.4
+I_Com_Sys = 74.5
+C_Programming = 79.4
+print("--- Your Score ---")
+print("Foundation English :",F_English)
+print("General Business :",G_Business)
+print("Introduction to Computer Systems :",I_Com_Sys)
+print("Computer Programming :",C_Programming)
