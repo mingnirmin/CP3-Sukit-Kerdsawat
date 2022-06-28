cff2bcb8da56a444a02b3755bade26d90539a160
oomsinsukit <oomsinsukit@gmail.com>
2022-06-28 18:31:45 +0700

    Add files via upload

---
+++ Lecture50.py
@@ -0,0 +1,13 @@
+def Number1(x,y):
+    print("Plus = ",x+y)
+def Number2(x,y):
+    print("Minus = ",x-y)
+def Number3(x,y):
+    print("Multiplied = ",x*y)
+def Number4(x,y):
+    print("Divide = ",x/y)
+
+Number1(80,20)
+Number2(80,20)
+Number3(80,20)
+Number4(80,20)
